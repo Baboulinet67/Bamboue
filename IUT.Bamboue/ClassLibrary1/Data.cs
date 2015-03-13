d1aab001506d8344cc74ade030fc835fe6a30380
miskin67120 <simon.uricher@gmail.com>
2015-03-13 11:26:29 +0100

    Commit de base

---
+++ IUT.Bamboue/ClassLibrary1/Data.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IUT.Bamboue.Outils;
+using System.Data;
+using System.Data.OleDb;
+using System.Configuration;
+
+namespace IUT.Bamboue.Data
+{
+    public class DataBamboue
+    {
+        ConnectionStringSettings connec = new ConnectionStringSettings("Bamboue", ConfigurationManager.ConnectionStrings["BamboueConnectionString"].ConnectionString, "System.Data.OleDb");
+    }
+}
